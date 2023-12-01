23e9a9956941006e38b92c1b71527852ce2bd734
Josh Hyatt <joshua.n.hyatt@gmail.com>
2023-11-30 17:28:40 -0700

    Add images/framebuffers

--- renderer/src/main/java/com/sector7/declarative_renderer/Renderer.kt
+++ renderer/src/main/java/com/sector7/declarative_renderer/Renderer.kt
@@ -1,25 +1,42 @@
 package com.sector7.declarative_renderer
 
-import android.opengl.GLES30.*
+import android.opengl.GLES30.GL_COLOR_BUFFER_BIT
+import android.opengl.GLES30.GL_DEPTH_BUFFER_BIT
+import android.opengl.GLES30.GL_DEPTH_TEST
+import android.opengl.GLES30.glClear
+import android.opengl.GLES30.glClearColor
+import android.opengl.GLES30.glEnable
+import android.opengl.GLES30.glLineWidth
+import android.opengl.GLES30.glViewport
 import com.sector7.math.Vec2i
 
 class Renderer {
     private class MeshDraw(val mesh: MeshId, val uniforms: List<UniformSet>, val pipeline: Pipeline)
     private class PendingShader(val id: ShaderId, val source: ShaderSource)
     private class PendingMesh(val id: MeshId, val mesh: Mesh)
+    private class PendingImage(val id: ImageId, val dimensions: Vec2i)
     private class PendingUniform(val id: UniformId, val uniform: UniformLocation)
+    private class PendingFramebuffer(
+        val id: FramebufferId, val colorTargets: List<ImageId>, val depthStencilTarget: ImageId?
+    )
 
     private val shaderIdGenerator = ShaderId.Generator()
     private val meshIdGenerator = MeshId.Generator()
     private val uniformIdGenerator = UniformId.Generator()
+    private val imageIdGenerator = ImageId.Generator()
+    private val framebufferIdGenerator = FramebufferId.Generator()
 
     private val shaders = HashMap<ShaderId, ShaderObject>()
     private val uniforms = HashMap<UniformId, UniformObject>()
     private val meshes = HashMap<MeshId, MeshObject>()
+    private val images = HashMap<ImageId, ImageObject>()
+    private val framebuffers = HashMap<FramebufferId, FramebufferObject>()
 
     private val pendingShaders = mutableListOf<PendingShader>()
     private val pendingMeshes = mutableListOf<PendingMesh>()
     private val pendingUniforms = mutableListOf<PendingUniform>()
+    private val pendingImages = mutableListOf<PendingImage>()
+    private val pendingFramebuffers = mutableListOf<PendingFramebuffer>()
 
     fun newShader(source: ShaderSource) = synchronized(this) {
         shaderIdGenerator.next().also { id ->
@@ -32,6 +49,19 @@ class Renderer {
             pendingMeshes.add(PendingMesh(id, mesh))
         }
     }
+
+    fun newImage(dimensions: Vec2i) = synchronized(this) {
+        imageIdGenerator.next().also { id ->
+            pendingImages.add(PendingImage(id, dimensions))
+        }
+    }
+
+    fun newFramebuffer(colorTargets: List<ImageId>, depthStencilTarget: ImageId?) =
+        synchronized(this) {
+            framebufferIdGenerator.next().also { id ->
+                pendingFramebuffers.add(PendingFramebuffer(id, colorTargets, depthStencilTarget))
+            }
+        }
 
     fun clearMeshes() {
         // TODO This is no longer that important
@@ -48,6 +78,7 @@ class Renderer {
         glClearColor(0f, 0f, 0f, 1f)
         glLineWidth(4f)
         glEnable(GL_DEPTH_TEST)
+        pipeline.textureSlots.forEach { (slot, image) -> images[image]!!.bind(slot) }
     }
 
     fun executePass(pass: RenderPass) {
@@ -56,7 +87,7 @@ class Renderer {
             it.value.map { draw -> MeshDraw(draw.mesh, draw.uniforms, draw.pipeline) }
         }
 
-        pass.framebuffer.bind()
+        framebuffers[pass.framebuffer]!!.bind()
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
         for (shaderId in draws.keys) {
             shaders[shaderId]!!.use()
@@ -67,6 +98,7 @@ class Renderer {
                     when (val value = uniformSet.value) {
                         is Vec3fUniform -> uniform.setVec3f(value.value)
                         is Mat4fUniform -> uniform.setMat4f(value.value)
+                        is ImageUniform -> uniform.setImage(value.slot)
                     }
                 }
                 meshes[draw.mesh]!!.draw()
@@ -83,25 +115,41 @@ class Renderer {
             pendingUniforms.forEach {
                 uniforms[it.id] = shaders[it.uniform.shader]!!.getUniform(it.uniform.name)
             }
+            pendingImages.forEach { images[it.id] = ImageObject.new(it.dimensions) }
+            pendingFramebuffers.forEach { framebuffers[it.id] = FramebufferObject.new() }
             pendingShaders.clear()
             pendingMeshes.clear()
             pendingUniforms.clear()
+            pendingImages.clear()
+            pendingFramebuffers.clear()
         }
         passes.forEach(::executePass)
     }
 }
 
-data class Pipeline(val shader: ShaderId, val viewport: Vec2i)
+data class Pipeline(
+    val shader: ShaderId, val textureSlots: HashMap<Int, ImageId>, val viewport: Vec2i
+)
 
 data class DrawCommand(val pipeline: Pipeline, val mesh: MeshId, val uniforms: List<UniformSet>)
 
 data class RenderPass(val framebuffer: FramebufferId, val draws: List<DrawCommand>)
 
 data class FramebufferId(private val id: Int) {
-    fun bind() = glBindFramebuffer(GL_FRAMEBUFFER, id)
-
     companion object {
         val default = FramebufferId(0)
+    }
+
+    internal class Generator {
+        private var id = 0
+        fun next() = FramebufferId(id++)
+    }
+}
+
+data class ImageId(private val id: Int) {
+    internal class Generator {
+        private var id = 0
+        fun next() = ImageId(id++)
     }
 }
 
--- renderer/src/main/java/com/sector7/declarative_renderer/Shader.kt
+++ renderer/src/main/java/com/sector7/declarative_renderer/Shader.kt
@@ -1,8 +1,34 @@
 package com.sector7.declarative_renderer
 
-import android.opengl.GLES30.*
+import android.opengl.GLES30.GL_FRAGMENT_SHADER
+import android.opengl.GLES30.GL_FRAMEBUFFER
+import android.opengl.GLES30.GL_RGBA
+import android.opengl.GLES30.GL_TEXTURE0
+import android.opengl.GLES30.GL_TEXTURE_2D
+import android.opengl.GLES30.GL_UNSIGNED_INT
+import android.opengl.GLES30.GL_VERTEX_SHADER
+import android.opengl.GLES30.glActiveTexture
+import android.opengl.GLES30.glAttachShader
+import android.opengl.GLES30.glBindFramebuffer
+import android.opengl.GLES30.glBindTexture
+import android.opengl.GLES30.glCompileShader
+import android.opengl.GLES30.glCreateProgram
+import android.opengl.GLES30.glCreateShader
+import android.opengl.GLES30.glDeleteShader
+import android.opengl.GLES30.glGenFramebuffers
+import android.opengl.GLES30.glGenTextures
+import android.opengl.GLES30.glGetShaderInfoLog
+import android.opengl.GLES30.glGetUniformLocation
+import android.opengl.GLES30.glLinkProgram
+import android.opengl.GLES30.glShaderSource
+import android.opengl.GLES30.glTexImage2D
+import android.opengl.GLES30.glUniform1i
+import android.opengl.GLES30.glUniform3f
+import android.opengl.GLES30.glUniformMatrix4fv
+import android.opengl.GLES30.glUseProgram
 import android.util.Log
 import com.sector7.math.Mat4f
+import com.sector7.math.Vec2i
 import com.sector7.math.Vec3f
 
 data class ShaderSource(val vertexShader: String, val fragmentShader: String)
@@ -11,6 +37,7 @@ data class UniformLocation(val shader: ShaderId, val name: String)
 sealed class UniformValue
 class Vec3fUniform(val value: Vec3f) : UniformValue()
 class Mat4fUniform(val value: Mat4f) : UniformValue()
+class ImageUniform(val slot: Int) : UniformValue()
 
 data class UniformSet(val uniform: UniformId, val value: UniformValue)
 
@@ -45,4 +72,39 @@ internal class ShaderObject(private val program: Int) {
 internal class UniformObject(private val location: Int) {
     fun setVec3f(x: Vec3f) = glUniform3f(location, x.x, x.y, x.z)
     fun setMat4f(x: Mat4f) = glUniformMatrix4fv(location, 1, false, x.asColumnMajorArray, 0)
+    fun setImage(slot: Int) = glUniform1i(location, slot)
+}
+
+internal class ImageObject(private val image: Int) {
+    fun bind(slot: Int) {
+        glActiveTexture(GL_TEXTURE0 + slot)
+        glBindTexture(GL_TEXTURE_2D, image)
+    }
+
+    companion object {
+        fun new(dimensions: Vec2i): ImageObject {
+            val image = intArrayOf(0).also { glGenTextures(1, it, 0) }[0]
+            glBindTexture(GL_TEXTURE_2D, image)
+            glTexImage2D(
+                GL_TEXTURE_2D,
+                0,
+                GL_RGBA,
+                dimensions.x,
+                dimensions.y,
+                0,
+                GL_RGBA,
+                GL_UNSIGNED_INT,
+                null
+            )
+            return ImageObject(image)
+        }
+    }
+}
+
+internal class FramebufferObject(private val framebuffer: Int) {
+    fun bind() = glBindFramebuffer(GL_FRAMEBUFFER, framebuffer)
+
+    companion object {
+        fun new() = FramebufferObject(intArrayOf(0).also { glGenFramebuffers(1, it, 0) }[0])
+    }
 }
